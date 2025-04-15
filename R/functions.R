8639c9ffe808b90ee22b2df2527c47519469332b
Vince Buffalo <vince.buffalo@gatesfoundation.org>
2025-04-15 14:48:52 -0700

    More additions to quarto doc

--- R/functions.R
+++ R/functions.R
@@ -1,11 +1,37 @@
 # --
+library(assertthat)
 
+# Beta-Poisson function (no immune scaling)
+beta_poisson <- function(dose, alpha, N50) {
+    1 - (1 + dose * (2^(1 / alpha) - 1) / N50)^(-alpha)
+}
 
 # Beta-Poisson dose-response function with immune scaling
-p_outcome_given_dose <- function(dose, alpha, N50, gamma, CoP) {
-    scale_factor <- (2^(1 / alpha) - 1)
-    base <- 1 + dose * scale_factor / N50
-    exponent <- -alpha / (CoP^gamma)
-    p <- 1 - base^exponent
-    return(p)
+beta_poisson_CoP <- function(dose, alpha, N50, gamma, CoP) {
+    1 - (1 + dose * (2^(1 / alpha) - 1) / N50)^(-alpha / CoP^gamma)
+}
+
+p_outcome_given_dose <- function(dose = 1e4, CoP = 1, outcome = "fever | dose", param_list) {
+    if (outcome == "fever | inf") {
+        # fever | info is calculated from probability of each outcome
+        p_fever <- p_outcome_given_dose(dose = dose, CoP = CoP, outcome = "fever | dose", param_list = param_list)
+        p_inf <- p_outcome_given_dose(dose = dose, CoP = CoP, outcome = "inf | dose", param_list = param_list)
+        return(p_fever / p_inf)
+    }
+
+    assert_that(outcome %in% names(param_list), msg = paste("Missing outcome:", outcome))
+
+    pars <- param_list[[outcome]]
+
+    return(beta_poisson_CoP(
+        dose  = dose,
+        alpha = pars$alpha,
+        N50   = pars$N50,
+        gamma = pars$gamma,
+        CoP   = CoP
+    ))
+}
+
+make_CoP_labels <- function(x) {
+    sprintf("CoP = %d", x)
 }
